e6502f4cb04a6e54054d7f05209e0bf048447caf
maddy00o7 <149822847+maddy00o7@users.noreply.github.com>
2024-04-17 16:41:20 +0530

    added code to fetch the latest comment and its a dynamically

--- src/components/ApplicationPipelineList.js
+++ src/components/ApplicationPipelineList.js
@@ -1,193 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import "./ApplicationPipelineList.css"; // Import the CSS file here
-
-// function ApplicationPipelineList() {
-//   const [applications, setApplications] = useState([]);
-//   const [selectedApplication, setSelectedApplication] = useState("");
-//   const [latestPipeline, setLatestPipeline] = useState(null);
-//   const [pipelineStatus, setPipelineStatus] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [success, setSuccess] = useState(false);
-
-//   useEffect(() => {
-//     // Fetch list of applications
-//     axios
-//       .get(
-//         "http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications"
-//       )
-//       .then((response) => {
-//         setApplications(response.data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching application list:", error);
-//       });
-//   }, []);
-
-//   useEffect(() => {
-//     if (!selectedApplication) return;
-
-//     // Fetch latest pipeline for selected application
-//     axios
-//       .get(
-//         `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications/${selectedApplication}/pipelines`
-//       )
-//       .then((response) => {
-//         // Assuming response.data is an array of pipelines
-//         // You can sort the pipelines based on timestamp and get the latest one
-//         const sortedPipelines = response.data.sort(
-//           (a, b) => new Date(b.startTime) - new Date(a.startTime)
-//         );
-//         const latest = sortedPipelines[0];
-//         setLatestPipeline(latest);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching pipelines:", error);
-//       });
-//   }, [selectedApplication]);
-
-//   const handleApplicationChange = (event) => {
-//     setSelectedApplication(event.target.value);
-//   };
-
-//   const handlePipelineTrigger = () => {
-//     if (!latestPipeline) {
-//       console.error("No latest pipeline available");
-//       return;
-//     }
-
-//     // Set loading state to true
-//     setLoading(true);
-
-//     // Trigger latest pipeline for selected application
-//     axios
-//       .post(
-//         `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/pipelines/${selectedApplication}/${latestPipeline.name}`
-//       )
-//       .then((response) => {
-//         console.log("Pipeline Triggered:", response.data);
-
-//         // Start polling for pipeline status
-//         pollPipelineStatus(latestPipeline.id);
-//       })
-//       .catch((error) => {
-//         console.error("Error triggering pipeline:", error);
-//         // Set loading state to false on error
-//         setLoading(false);
-//       });
-//   };
-
-//   const pollPipelineStatus = (pipelineId) => {
-//     // Polling function
-//     const intervalId = setInterval(() => {
-//       // Fetch pipeline status using the retrieved pipeline ID
-//       axios
-//         .get(
-//           `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/pipelines/${pipelineId}`
-//         )
-//         .then((response) => {
-//           console.log("Pipeline Status:", response.data.status);
-//           const executionStatus = response.data.status;
-//           setPipelineStatus(executionStatus);
-//           if (executionStatus === "SUCCEEDED") {
-//             clearInterval(intervalId);
-//             setLoading(false);
-//             setSuccess(true);
-//           } else if (executionStatus === "FAILED") {
-//             clearInterval(intervalId);
-//             setLoading(false);
-//             setSuccess(false);
-//           } else if (executionStatus === "CANCELED") {
-//             clearInterval(intervalId);
-//             setLoading(false);
-//             setSuccess(false);
-//           }
-//         })
-//         .catch((error) => {
-//           console.error("Error fetching pipeline status:", error);
-//           // Set loading state to false on error
-//           setLoading(false);
-//         });
-//     }, 5000); // Poll every 5 seconds
-//   };
-
-//   return (
-//     <div className="container">
-//       <img
-//         src="https://www.minfytech.com/wp-content/uploads/2022/04/logo-minfy.png"
-//         alt="MinfyTech Logo"
-//         className="logo"
-//       />
-//       <div className="box">
-//         <h2 className="heading">Application list</h2>
-//         <select
-//           className="dropdown"
-//           value={selectedApplication}
-//           onChange={handleApplicationChange}
-//         >
-//           <option key={"no-select"} value="">
-//             Select an Application
-//           </option>
-//           {applications.map((application) => (
-//             <option key={application.id} value={application.id}>
-//               {application.name}
-//             </option>
-//           ))}
-//         </select>
-
-//         {selectedApplication && (
-//           <div className="pipeline-table">
-//             <h3>Pipelines</h3>
-//             {latestPipeline && (
-//               <table>
-//                 <thead>
-//                   <tr>
-//                     <th>Pipeline Name</th>
-//                     <th>Action</th>
-//                     <th>Status</th>
-//                   </tr>
-//                 </thead>
-//                 <tbody>
-//                   <tr>
-//                     <td>{latestPipeline.name}</td>
-//                     <td>
-//                       <button
-//                         className="trigger-button"
-//                         disabled={loading}
-//                         onClick={handlePipelineTrigger}
-//                       >
-//                         Trigger
-//                       </button>
-//                     </td>
-//                     <td>
-//                       {loading ? (
-//                         <span>
-//                           Running... <i className="fa fa-spinner fa-spin"></i>
-//                         </span>
-//                       ) : success ? (
-//                         <span>
-//                           <i className="fa fa-check-circle"></i> Success
-//                         </span>
-//                       ) : (
-//                         <span>{pipelineStatus}</span>
-//                       )}
-//                     </td>
-//                   </tr>
-//                 </tbody>
-//               </table>
-//             )}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ApplicationPipelineList;
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./ApplicationPipelineList.css"; // Import the CSS file here
+import "./ApplicationPipelineList.css";
 
 function ApplicationPipelineList() {
   const [applications, setApplications] = useState([]);
@@ -197,14 +10,16 @@ function ApplicationPipelineList() {
   const [pipelineStatus, setPipelineStatus] = useState("");
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [pipelineCount, setPipelineCount] = useState(0); // New state to hold the number of pipelines
+  const [pipelineCount, setPipelineCount] = useState(0);
+  const [showEndpointTable, setShowEndpointTable] = useState(false);
+  const [endpointData, setEndpointData] = useState([]);
+  const [pipelineTriggered, setPipelineTriggered] = useState(false);
+  const [latestCommit, setLatestCommit] = useState(null);
+  const [showCommitDetails, setShowCommitDetails] = useState(false);
 
   useEffect(() => {
-    // Fetch list of applications
     axios
-      .get(
-        "http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications"
-      )
+      .get("http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications")
       .then((response) => {
         setApplications(response.data);
       })
@@ -216,14 +31,10 @@ function ApplicationPipelineList() {
   useEffect(() => {
     if (!selectedApplication) return;
 
-    // Fetch pipelines for selected application
     axios
-      .get(
-        `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications/${selectedApplication}/pipelines`
-      )
+      .get(`http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/applications/${selectedApplication}/pipelines`)
       .then((response) => {
         setPipelines(response.data);
-        // Set the number of pipelines
         setPipelineCount(response.data.length);
       })
       .catch((error) => {
@@ -245,35 +56,37 @@ function ApplicationPipelineList() {
       return;
     }
 
-    // Set loading state to true
+    const selectedPipelineObj = pipelines.find(pipeline => pipeline.name === selectedPipeline);
+
+    if (!selectedPipelineObj) {
+      console.error("Selected pipeline not found");
+      return;
+    }
+
     setLoading(true);
 
-    // Trigger selected pipeline for selected application
     axios
-      .post(
-        `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/pipelines/${selectedApplication}/${selectedPipeline}`
-      )
+      .post(`http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/pipelines/${selectedApplication}/${selectedPipelineObj.name}`)
       .then((response) => {
         console.log("Pipeline Triggered:", response.data);
-
-        // Start polling for pipeline status
-        pollPipelineStatus(response.data.id);
+        pollPipelineStatus(response.data.ref);
+        setPipelineTriggered(true);
       })
       .catch((error) => {
         console.error("Error triggering pipeline:", error);
-        // Set loading state to false on error
         setLoading(false);
       });
   };
 
   const pollPipelineStatus = (pipelineId) => {
-    // Polling function
     const intervalId = setInterval(() => {
-      // Fetch pipeline status using the retrieved pipeline ID
+      if (!selectedPipeline) {
+        clearInterval(intervalId);
+        return;
+      }
+
       axios
-        .get(
-          `http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084/pipelines/${pipelineId}`
-        )
+        .get(`http://afea00e0026e7426ebc40b119373ee6f-835370818.us-east-1.elb.amazonaws.com:8084${pipelineId}`)
         .then((response) => {
           console.log("Pipeline Status:", response.data.status);
           const executionStatus = response.data.status;
@@ -282,7 +95,11 @@ function ApplicationPipelineList() {
             clearInterval(intervalId);
             setLoading(false);
             setSuccess(true);
-          } else if (executionStatus === "FAILED" || executionStatus === "CANCELED") {
+          } else if (
+            executionStatus === "FAILED" ||
+            executionStatus === "CANCELED" ||
+            executionStatus === "TERMINAL"
+          ) {
             clearInterval(intervalId);
             setLoading(false);
             setSuccess(false);
@@ -290,12 +107,32 @@ function ApplicationPipelineList() {
         })
         .catch((error) => {
           console.error("Error fetching pipeline status:", error);
-          // Set loading state to false on error
           setLoading(false);
         });
-    }, 5000); // Poll every 5 seconds
+    }, 5000);
   };
 
+  const getEndpoint = (pipeline) => {
+    switch (pipeline) {
+      case "dev":
+        return "http://a915a80b42c0c4c7eac7a3f5aa619dea-627576352.us-east-1.elb.amazonaws.com";
+      case "uat":
+        return "http://a4accc2b34f81444fbaf6652837d97cc-1181325399.us-east-1.elb.amazonaws.com";
+      case "prod":
+        return "http://a57d4da36efd34c56bfcabb3310af27d-196101690.us-east-1.elb.amazonaws.com";
+      default:
+        return "";
+    }
+  };
+
+  const getCommitLink = () => {
+    const username = '********'; // Replace with your GitHub username
+    const repo = 'private-run'; // Replace with your GitHub repository name
+    const accessToken = '**token**'; // Replace with your GitHub access token
+    
+    // Generate the GitHub URL based on the selected pipeline
+    return selectedPipeline ? `https://github.com/${username}/${repo}/commits/${selectedPipeline}?access_token=${accessToken}` : "";
+  };
   return (
     <div className="container">
       <img
@@ -369,24 +206,40 @@ function ApplicationPipelineList() {
                 </tr>
               </tbody>
             </table>
-            <button
-              className="trigger-button"
-              disabled={loading}
-              onClick={handlePipelineTrigger}
-            >
-              Trigger Pipeline
-            </button>
+            <div>
+              <button
+                className="trigger-button"
+                disabled={loading}
+                onClick={handlePipelineTrigger}
+              >
+                Trigger Pipeline
+              </button>
+            </div>
+            <div>
+              {pipelineTriggered && success && (
+                <button
+                  className="view-endpoint-button"
+                  onClick={() => window.open(getEndpoint(selectedPipeline), "_blank")}
+                >
+                  View application page
+                </button>
+              )}
+            </div>
+            <div>
+              {pipelineTriggered && success && (
+                <button
+                  className="view-endpoint-button"
+                  onClick={() => window.open(getCommitLink(selectedPipeline), "_blank")}
+                >
+                  View Commit Details
+                </button>
+              )}
+            </div>
           </div>
         )}
-      </div>
-      
-      {/* Display the number of pipelines */}
-      <div className="pipeline-count">
-        <h3>Total Pipelines: {pipelineCount}</h3>
       </div>
     </div>
   );
 }
 
 export default ApplicationPipelineList;
-
